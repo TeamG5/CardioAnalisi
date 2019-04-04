8ea6a6841588921bb842fae996b3353d66ba6612
lucaterzino56 <lucaterzino56@gmail.com>
2019-04-04 10:15:11 +0200

    Created  two methods : -Battito>0 -Battito<0

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -8,5 +8,23 @@ namespace CardioLibrary
 {
     public class DataCardio
     {
+        public static string BattitoMaggiore(int battito)
+        {
+            string msg = "";
+            if (battito > 0 )
+            {
+                msg = "Battito accettabile";
+            }
+            return msg;
+        }
+        public static string BattitoMinore(int battito)
+        {
+            string msg = "";
+            if (battito < 0)
+            {
+                msg = "Battito non accettabile";
+            }
+            return msg;
+        }
     }
 }
--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataCardio;
 
 namespace DataCardio.Test
 {
@@ -7,9 +8,21 @@ namespace DataCardio.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMaggioredi0()
         {
+            int battito = 50;
+            string Risp = "";
 
+            Risp = CardioLibrary.DataCardio.BattitoMaggiore(battito);
+        }
+
+        [TestMethod]
+        public void TestMinoredi0()
+        {
+            int battito = -5;
+            string Risp = "";
+
+            Risp = CardioLibrary.DataCardio.BattitoMinore(battito);
         }
     }
 }
