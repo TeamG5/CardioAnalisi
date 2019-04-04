34ec5f6d457e4c2270179630197b9e57de3448fa
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-04 10:11:52 +0200

    Created one metod e three tests to prove is functionality

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -8,5 +8,22 @@ namespace CardioLibrary
 {
     public class DataCardio
     {
+        public static string Frequenza(double età)
+        {
+            string msg = "";
+            double frequenza_massima = 0;
+            if (età<=0)
+            {
+                msg = "attenzione l'età non può essere minore o uguale a 0";
+            }
+            if(età>0)
+            {
+                
+                frequenza_massima = 220 - età;
+
+                msg = Convert.ToString(frequenza_massima);
+            }
+            return msg;
+        }
     }
 }
--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataCardio;
+
 
 namespace DataCardio.Test
 {
@@ -9,6 +11,26 @@ namespace DataCardio.Test
         [TestMethod]
         public void TestMethod1()
         {
+            double età = 0;
+            string asp = "attenzione l'età non può essere minore o uguale a 0";
+            string ott = CardioLibrary.DataCardio.Frequenza(età);
+            Assert.AreEqual(asp,ott);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double età = 20;
+            string asp = "200";
+            string ott = CardioLibrary.DataCardio.Frequenza(età);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            double età = -30;
+            string asp = "attenzione l'età non può essere minore o uguale a 0";
+            string ott = CardioLibrary.DataCardio.Frequenza(età);
+            Assert.AreEqual(asp, ott);
         }
     }
 }
