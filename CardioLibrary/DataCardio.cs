fc79be194fb51609416bdd5aa7b95fce895204d5
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-11 09:17:19 +0200

    metodo finito. Correzione dei test

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -8,6 +8,23 @@ namespace CardioLibrary
 {
     public class DataCardio
     {
-
+        static string CalcoloFrequenza(int a)
+        {
+            string risposta = "";
+            if(a<=0)
+            {
+                risposta = "la tua età non può essere minore o uguale a 0";
+            }
+            else
+            {
+                int frequenza = 220 - a;
+                double fmax = frequenza * 0.9;
+                double fmin = frequenza * 0.7;
+                Convert.ToString(fmax);
+                Convert.ToString(fmin);
+                risposta=$"La tua frequenza massima è {fmax} e quella minima è {fmin}";
+            }
+            return risposta;
+        }
     }
 }
--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -6,9 +6,34 @@ namespace DataCardio.Test
     [TestClass]
     public class UnitTest1
     {
+        //test età uguale a 0
         [TestMethod]
         public void TestMethod1()
-        { 
+        {
+            int eta = 0;
+            string calcolo = CardioLibrary.DataCardio.CalcoloFrequenza(eta);
+            string risp_aspettata = "la tua età non può essere minore o uguale a 0";
+            Assert.AreEqual(calcolo, risp_aspettata);
+        }
+
+        //test età minore di 0
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int eta = -5;
+            string calcolo = CardioLibrary.DataCardio.CalcoloFrequenza(eta);
+            string risp_aspettata = "la tua età non può essere minore o uguale a 0";
+            Assert.AreEqual(calcolo, risp_aspettata);
+        }
+
+        //test età maggiore di 0
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int eta = 20;
+            string calcolo = CardioLibrary.DataCardio.CalcoloFrequenza(eta);
+            string risp_aspettata = "La tua frequenza massima è 180 e quella minima è 140";
+            Assert.AreEqual(calcolo, risp_aspettata);
         }
     }
 }
