7f48ed8ab9f7b874bc79b730abc24e1e3a2d355b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-17 09:11:32 +0200

    terminato il secondo punto con test e metono, iniziato il terzo punto

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -8,10 +8,11 @@ namespace CardioLibrary
 {
     public class DataCardio
     {
-        static string CalcoloFrequenza(int a)
+        //calcolo della frequenza in base all'età per un'allenamento efficace
+        public static string CalcoloFrequenza1(int a)
         {
             string risposta = "";
-            if(a<=0)
+            if (a <= 0)
             {
                 risposta = "la tua età non può essere minore o uguale a 0";
             }
@@ -24,6 +25,51 @@ namespace CardioLibrary
                 Convert.ToString(fmin);
                 risposta=$"La tua frequenza massima è {fmax} e quella minima è {fmin}";
             }
+            return risposta;
+        }
+
+        //Calcolo della frequenza a riposo
+        public static string FrequenzaRiposo(int a)
+        {
+            string risposta = "";
+            if (a == 0)
+            {
+                risposta = "Errore";
+            }
+            if (a < 60 && a > 0)
+            {
+                risposta = "Sei bradicardico";
+            }
+            if (a >= 60 && a <= 100)
+            {
+                risposta = "Il tuo battito è nella norma";
+            }
+            if (a > 100)
+            {
+                risposta = "Sei tachicardico";
+            }
+            return risposta;
+        }
+
+        //Calcolo delle calorie bruciate
+        public static string CalorieBruciate(double F, double P, double T, string sesso)
+        {
+            string risposta = "";
+            double calorie = 0;
+            if (F == 0 || P == 0 || T == 0)
+            {
+                risposta = "Errore";
+            }
+            if (sesso == "")
+            {
+                risposta = "Errore";
+            }
+
+            if (sesso == "uomo")
+            {
+                
+            }
+
             return risposta;
         }
     }
---
+++ DataCardio.Test/DataCardioTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataCardio;
+
+namespace DataCardio.Test
+{
+    [TestClass]
+    public class DataCardioTest
+    {
+        //calcolo della frequenza in base all'età per un'allenamento efficace
+        //test età uguale a 0
+        [TestMethod]
+        public void Efficace1()
+        {
+            int eta = 0;
+            string calcolo = CardioLibrary.DataCardio.CalcoloFrequenza1(eta);
+            string risp_aspettata = "la tua età non può essere minore o uguale a 0";
+            Assert.AreEqual(calcolo, risp_aspettata);
+        }
+
+        //test età minore di 0
+        [TestMethod]
+        public void Efficace2()
+        {
+            int eta = -5;
+            string calcolo = CardioLibrary.DataCardio.CalcoloFrequenza1(eta);
+            string risp_aspettata = "la tua età non può essere minore o uguale a 0";
+            Assert.AreEqual(calcolo, risp_aspettata);
+        }
+
+        //test età maggiore di 0
+        [TestMethod]
+        public void Efficace3()
+        {
+            int eta = 20;
+            string calcolo = CardioLibrary.DataCardio.CalcoloFrequenza1(eta);
+            string risp_aspettata = "La tua frequenza massima è 180 e quella minima è 140";
+            Assert.AreEqual(calcolo, risp_aspettata);
+        }
+
+
+        //Calcolo della frequenza a riposo
+        //test battito uguale o minore di 0
+        [TestMethod]
+        public void Riposo1()
+        {
+            int FrequenzaCardiaca = 0;
+            string calcolo = CardioLibrary.DataCardio.FrequenzaRiposo(FrequenzaCardiaca);
+            string risp_aspettata = "Errore";
+            Assert.AreEqual(calcolo, risp_aspettata);
+        }
+
+        //test battito nella norma
+        [TestMethod]
+        public void Riposo2()
+        {
+            int FrequenzaCardiaca = 60;
+            string calcolo = CardioLibrary.DataCardio.FrequenzaRiposo(FrequenzaCardiaca);
+            string risp_aspettata = "Il tuo battito è nella norma";
+            Assert.AreEqual(calcolo, risp_aspettata);
+        }
+
+        //test ber bradicardia
+        [TestMethod]
+        public void Riposo3()
+        {
+            int FrequenzaCardiaca = 50;
+            string calcolo = CardioLibrary.DataCardio.FrequenzaRiposo(FrequenzaCardiaca);
+            string risp_aspettata = "Sei bradicardico";
+            Assert.AreEqual(calcolo, risp_aspettata);
+        }
+
+        //test per tachicardia
+        [TestMethod]
+        public void Riposo4()
+        {
+            int FrequenzaCardiaca = 120;
+            string calcolo = CardioLibrary.DataCardio.FrequenzaRiposo(FrequenzaCardiaca);
+            string risp_aspettata = "Sei tachicardico";
+            Assert.AreEqual(calcolo, risp_aspettata);
+        }
+
+
+        //Calcolo delle calorie bruciate
+        [TestMethod]
+        public void Calorie1()
+        {
+            string sesso = "uomo";
+            double FrequenzaCardiacaMedia = 0;
+            double  Peso= 0;
+            double DurataAllenamento = 0;
+
+            string Calorie = CardioLibrary.DataCardio.CalorieBruciate(FrequenzaCardiacaMedia, Peso, DurataAllenamento, sesso);
+            string risp_aspettata = "Sei tachicardico";
+            Assert.AreEqual(Calorie, risp_aspettata);
+        }
+    }
+}
