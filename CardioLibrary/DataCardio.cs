442b377f2afe751d570cb510b6314996feb2cc89
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-17 09:14:52 +0200

    Implementati test es 2

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -11,9 +11,9 @@ namespace CardioLibrary
         public static string BattitoMaggiore(int battito)
         {
             string msg = "";
-            if (battito > 0 )
+            if (battito > 0)
             {
-                msg = "Battito accettabile";
+                msg = "il battito è nella norma";
             }
             return msg;
         }
@@ -22,9 +22,49 @@ namespace CardioLibrary
             string msg = "";
             if (battito < 0)
             {
-                msg = "Battito non accettabile";
+                msg = "Battito non è nella norma!!ATTENZIONE";
             }
             return msg;
+        }
+
+        public static string Frequenza(double età)
+        {
+            string msg = "";
+            double frequenza_massima = 0;
+            double frequenza_minima = 0;
+            double frequenza = 0;
+            if (età <= 0)
+            {
+                msg = "ATTENZIONE!! l'età non può essere minore o uguale a 0";
+            }
+            if (età > 0)
+            {
+                frequenza = 220 - età;
+                frequenza_massima = frequenza * 0.9;
+                frequenza_minima = frequenza * 0.7;
+                Convert.ToString(frequenza_massima);
+                Convert.ToString(frequenza_minima);
+                msg = $"la frequenza massima è {frequenza_massima} e la frequenza minima è {frequenza_minima}";
+            }
+            return msg;
+        }
+
+        public static string Valori(int Frequenza_Cardiaca)
+        {
+            string messaggio = "";
+            if (Frequenza_Cardiaca < 60)
+            {
+                messaggio = "Data la sua frequenza cardiaca lei è una persona Bradicardica";
+            }
+            else if (Frequenza_Cardiaca > 120)
+            {
+                messaggio = "data la sua frequenza cardiaca lei è una persona Tachicardica";
+            }
+            else
+            {
+                messaggio = "La sua frequenza cardiaca è nella norma";
+            }
+            return messaggio;
         }
     }
 }
---
+++ DataCardio.Test/DataCardioTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataCardio;
+
+namespace DataCardio.Test
+{
+    [TestClass]
+    public class DataCardioTest
+    {
+        [TestMethod]
+        public void TestMaggioredi0()
+        {
+            int battito = 50;
+            string Risp = "";
+
+            Risp = CardioLibrary.DataCardio.BattitoMaggiore(battito);
+        }
+
+        [TestMethod]
+        public void TestMinoredi0()
+        {
+            int battito = -5;
+            string Risp = "";
+
+            Risp = CardioLibrary.DataCardio.BattitoMinore(battito);
+        }
+
+        [TestMethod]
+        public void TestEtà()
+        {
+            double età = 0;
+            string asp = "ATTENZIONE!!!l'età non può essere minore o uguale a 0";
+            string ott = CardioLibrary.DataCardio.Frequenza(età);
+            Assert.AreEqual(asp, ott);
+        }
+
+        [TestMethod]
+        public void TestEtà2()
+        {
+            double età = 20;
+            string asp = "la frequenza massima è 180 e la frequrnza minima è 140";
+            string ott = CardioLibrary.DataCardio.Frequenza(età);
+            Assert.AreEqual(asp, ott);
+        }
+
+        [TestMethod]
+        public void TestEtà3()
+        {
+            double età = -30;
+            string asp = "ATTENZIONE!!!l'età non può essere minore o uguale a 0";
+            string ott = CardioLibrary.DataCardio.Frequenza(età);
+            Assert.AreEqual(asp, ott);
+        }
+
+        [TestMethod]
+        public void TestFrequenza()
+        {
+            int Frequenza_Cardiaca = 70;
+            string asp = "Normale";
+            string ott = CardioLibrary.DataCardio.Valori(Frequenza_Cardiaca);
+            Assert.AreEqual(asp, ott);
+        }
+
+        [TestMethod]
+        public void TestTachicardia()
+        {
+            int Frequenza_Cardiaca = 120;
+            string asp = "Tachicardia";
+            string ott = CardioLibrary.DataCardio.Valori(Frequenza_Cardiaca);
+            Assert.AreEqual(asp, ott);
+        }
+
+        [TestMethod]
+        public void TestBradicardia()
+        {
+            int Frequenza_Cardiaca = 50;
+            string asp = "Bradicardia";
+            string ott = CardioLibrary.DataCardio.Valori(Frequenza_Cardiaca);
+            Assert.AreEqual(asp, ott);
+        }
+
+
+    }
+}
