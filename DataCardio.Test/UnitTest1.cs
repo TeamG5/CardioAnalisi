59e32e4c9d52cf46cd702617978d9da07963b488
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-05-02 10:05:53 +0200

    riaggiunto codice per problema di branches

--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -8,43 +8,178 @@ namespace DataCardio.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMaggioredi0()
+        public void Età_uguale_a_0()
         {
-            int battito = 5;
-            string rispostaaspettata = "Battito Accettabile";
-            string risposta = "";
-            risposta = CardioLibrary.DataCardio.BattitoMaggiore(battito);
-            Assert.AreEqual(risposta, rispostaaspettata);
+            double età = 0;
+            string asp = "attenzione l'età non può essere minore o uguale a 0";
+            string ott = CardioLibrary.DataCardio.Frequenza(età);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Età_maggiore_di_0()
+        {
+            double età = 20;
+            string asp = "la frequenza massima è 180 e la frequenza minima è 140";
+            string ott = CardioLibrary.DataCardio.Frequenza(età);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Età_minore_di_0()
+        {
+            double età = -30;
+            string asp = "attenzione l'età non può essere minore o uguale a 0";
+            string ott = CardioLibrary.DataCardio.Frequenza(età);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Frequenza_Cardiaca_maggiore_di_60_e_minore_i_100()
+        {
+            int Frequenza_Cardiaca = 70;
+            string asp = "Normale";
+            string ott = CardioLibrary.DataCardio.Valori(Frequenza_Cardiaca);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Frequenza_Cardiaca_maggiore_di_100()
+        {
+            int Frequenza_Cardiaca = 120;
+            string asp = "Tachicardia";
+            string ott = CardioLibrary.DataCardio.Valori(Frequenza_Cardiaca);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Frequenza_Cardiaca_minore_di_60()
+        {
+            int Frequenza_Cardiaca = 50;
+            string asp = "Bradicardia";
+            string ott = CardioLibrary.DataCardio.Valori(Frequenza_Cardiaca);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Frequenza_Cardiaca_uguale_a_0()
+        {
+            int Frequenza_Cardiaca = 0;
+            string asp = "Attenzione i valori non possono essere accettati";
+            string ott = CardioLibrary.DataCardio.Valori(Frequenza_Cardiaca);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Calorie_maschio()
+        {
+            string sesso = "Maschio";
+            int a = 50;
+            double p = 60;
+            double t = 60;
+            double f = 65;
+            string asp = "113,813575525813";
+            string ott = CardioLibrary.DataCardio.Calorie(sesso, a, p, t, f);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Calorie_maschio1()
+        {
+            string sesso = "";
+            int a = 50;
+            double p = 0;
+            double t = 0;
+            double f = 65;
+            string asp = "Attenzione i valori non possono essere accettati";
+            string ott = CardioLibrary.DataCardio.Calorie(sesso, a, p, t, f);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Calorie_femmina()
+        {
+            string sesso = "femmine";
+            int a = 40;
+            double p = 76;
+            double t = 29;
+            double f = 65;
+            string asp = "146,953202676864";
+            string ott = CardioLibrary.DataCardio.Calorie(sesso, a, p, t, f);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Calorie_femmina1()
+        {
+            string sesso = "femmine";
+            int a = 40;
+            double p = 76;
+            double t = 0;
+            double f = 0;
+            string asp = "Attenzione i valori non possono essere accettati";
+            string ott = CardioLibrary.DataCardio.Calorie(sesso, a, p, t, f);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Spesa_energetica_camminata()
+        {
+            string camminata = "camminata";
+            double km_percorsi = 7;
+            double kg_peso = 65;
+            string asp = "409,5";
+            string ott = CardioLibrary.DataCardio.Spesa(camminata, km_percorsi, kg_peso);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Spesa_energetica_camminata1()
+        {
+            string camminata = "camminata";
+            double km_percorsi = 0;
+            double kg_peso = 65;
+            string asp = "Attenzione i valori non possono essere accettati";
+            string ott = CardioLibrary.DataCardio.Spesa(camminata, km_percorsi, kg_peso);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Spesa_energetica_corsa()
+        {
+            string camminata = "corsa";
+            double km_percorsi = 12;
+            double kg_peso = 55;
+            string asp = "330";
+            string ott = CardioLibrary.DataCardio.Spesa(camminata, km_percorsi, kg_peso);
+            Assert.AreEqual(asp, ott);
+        }
+        [TestMethod]
+        public void Spesa_energetica_corsa1()
+        {
+            string camminata = "";
+            double km_percorsi = 12;
+            double kg_peso = 0;
+            string asp = "Attenzione i valori non possono essere accettati";
+            string ott = CardioLibrary.DataCardio.Spesa(camminata, km_percorsi, kg_peso);
+            Assert.AreEqual(asp, ott);
+        }
+        //Calcolo battiti con array
+        [TestMethod]
+        public void BattitiGiornata1()
+        {
+            int[] battiti = new int[5];
+            battiti[0] = 0;
+            battiti[1] = 0;
+            battiti[2] = 0;
+            battiti[3] = 0;
+            battiti[4] = 0;
+
+            string risp_battiti = CardioLibrary.DataCardio.BattitiGiornata(battiti);
+            string risp_aspettata = "Errore";
+            Assert.AreEqual(risp_battiti, risp_aspettata);
         }
 
         [TestMethod]
-        public void TestBattitoPiatto()
+        public void BattitiGiornata2()
         {
-            int battito = 0;
-            string rispostaaspettata = "Errore! Battito non rilevato.";
-            string risposta = "";
-            risposta = CardioLibrary.DataCardio.BattitoPiatto(battito);
-            Assert.AreEqual(risposta, rispostaaspettata);
-        }
+            int[] battiti = new int[5];
+            battiti[0] = 65;
+            battiti[1] = 62;
+            battiti[2] = 94;
+            battiti[3] = 75;
+            battiti[4] = 68;
 
-        [TestMethod]
-        public void TestEtaMaggiore()
-        {
-            int eta = 5;
-            string rispostaaspettata = "Età Accettata";
-            string risposta = "";
-            risposta = CardioLibrary.DataCardio.EtaMaggiore(eta);
-            Assert.AreEqual(risposta, rispostaaspettata);
-        }
-
-        [TestMethod]
-        public void TestEtaMaggiore1()
-        {
-            int eta = -5;
-            string rispostaaspettata = "Età Rifiutata";
-            string risposta = "";
-            risposta = CardioLibrary.DataCardio.EtaMinore(eta);
-            Assert.AreEqual(risposta, rispostaaspettata);
+            string risp_battiti = CardioLibrary.DataCardio.BattitiGiornata(battiti);
+            string risp_aspettata = $"La tua media giornaliera dei battiti cardiaci è di 72 bpm. Il tuo battito cardiaco a riposo è di 62 bpm. La variabilità del tuo battito cardiaco durante la giornata è di 32 bpm. L'ordine crescente dei battiti cardiaci durante la giornata è 62 65 68 75 94.";
+            Assert.AreEqual(risp_battiti, risp_aspettata);
         }
     }
 }
